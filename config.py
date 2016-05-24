50b54d3fa1c2db9c8a91fce739a05c8aeb365ed9
Alice <1399789151@qq.com>
2016-05-24 16:57:28 +0800

    flasky config and manage

---
+++ config.py
@@ -0,0 +1,47 @@
+#coding:utf-8
+import os
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+	SECERT_KEY='hard to guess string'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN=True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	#FLASKY_MAIL_SUBJECT_PREFIX='[flasky]'
+	#FLASKY_MAIL_SENDER=''
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+class DevelopmentConfig(Config):
+	DEBUG=True
+	SQLALCHEMY_DATABASE_URI=\
+	    'sqlite:///'+os.path.join(basedir,'data-dev.sqlite')
+
+
+    
+
+class TestingConfig(Config):
+	TESTING=True
+	SQLALCHEMY_DATABASE_URI=\
+	    'sqlite:///'+os.path.join(basedir,'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI=\
+	    'sqlite:///'+os.path.join(basedir,'data.sqlite')
+
+config={
+	'development':DevelopmentConfig,
+	'testing':TestingConfig,
+	'production':ProductionConfig,
+
+	'default':DevelopmentConfig
+}	
+
+
+		
+    	
+
+			
