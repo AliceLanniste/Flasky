af546ebbe2b70320d1050f97b0576f7f52c98d8c
Alice <1399789151@qq.com>
2016-05-25 09:17:15 +0800

    branch login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from werkzeug .security import generate_password_hash,check_password_hash
-
+from . import db
+from flask.ext.login import UserMixin
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -9,9 +10,10 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
-	__tablename__="users"
-	id=db.Column(db.Integer,primary_key=True)
+class User(UserMixin,db.Model):
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	email=db.Column(db.String(64),unique=True,index=True)
 	Username=db.Column(db.String(64),unique=True,index=True)
 	role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
 	password_hash=db.Column(db.String(128))
@@ -34,4 +36,9 @@ class User(db.Model):
 		return '<user %s>' % self.Username
 		
 
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
