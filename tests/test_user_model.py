e4e4e49b3cc77f126865548cc4739a1332ddad69
Alice <1399789151@qq.com>
2016-05-31 18:06:34 +0800

    login 2

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,2 +1,27 @@
 import unittest
-from app.models import User
+from app.models import User,Permission,Role,AnonymousUser
+
+
+class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+class UserModelTestCase(unittest.TestCase):
+    def test_roles_and_permissions(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+
